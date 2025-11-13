a6668f869df4437b01879cad12e4f0a7443e9cd3
ambatisainithin03 <ambatisainithin03@gmail.com>
2025-11-13 15:15:59 +0530

    finally project done....

--- Vehicle_Backend_Server/vehicle.js
+++ Vehicle_Backend_Server/vehicle.js
@@ -1,25 +1,41 @@
 const e = require("express");
-const app=e();
-const cors=require("cors")
+const app = e();
+const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const { newuser, login, createBooking, bookingtoadmin , getUserBookingStatus, updateBookingStatus,assignMechanic, markBookingDone,getMechanicBookings} = require("./Controller");
+const {
+  newuser,
+  login,
+  createBooking,
+  bookingtoadmin,
+  getUserBookingStatus,
+  updateBookingStatus,
+  assignMechanic,
+  markBookingDone,
+  getMechanicBookings,
+} = require("./Controller");
 const { verifyToken } = require("./middlewear");
 
 app.use(e.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
 
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>console.log("Database Connected...")) 
-.catch((err)=>console.log(err.message))
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database Connected..."))
+  .catch((err) => console.log(err.message));
 
-app.post('/newuser',newuser)
-app.post('/login',login)
-app.post('/createbooking',verifyToken,createBooking)
-app.get('/bookingtoadmin',verifyToken,bookingtoadmin)
+app.post("/newuser", newuser);
+app.post("/login", login);
+app.post("/createbooking", verifyToken, createBooking);
+app.get("/bookingtoadmin", verifyToken, bookingtoadmin);
 
-app.patch("/bookings/:id/status",updateBookingStatus);
-app.get("/userstatus",verifyToken,getUserBookingStatus);
+app.patch("/bookings/:id/status", updateBookingStatus);
+app.get("/userstatus", verifyToken, getUserBookingStatus);
 
-
-app.listen(3000,()=>{console.log("server running on http://localhost:3000")})
+app.listen(3000, () => {
+  console.log("server running on http://localhost:3000");
+});
